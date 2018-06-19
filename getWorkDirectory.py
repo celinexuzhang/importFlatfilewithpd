22d167af3026a835a786f66366c75b8c2c1a2de3
celinexuzhang <celinezhang1991@outlook.com>
2018-06-19 16:02:11 -0400

    ExploreCurrentWD

--- getWorkDirectory.py
+++ getWorkDirectory.py
@@ -1,3 +1,6 @@
+#to explore your current working directory. You can also do this natively in Python using the library os, which consists of miscellaneous operating system interfaces.
+#The first line of the following code imports the library os, the second line stores the name of the current directory in a string called wd and the third outputs the contents of the directory in a list to the shell.
+
 import os
 wd = os.getcwd()
 os.listdir(wd)
