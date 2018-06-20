1f3ac70b79c98e90dec3dff93306015ba31cdff7
celinexuzhang <celinezhang1991@outlook.com>
2018-06-19 20:37:09 -0400

    SaveDicttoPickle

--- SaveDicttoPickle.py
+++ SaveDicttoPickle.py
@@ -2,6 +2,7 @@ import pickle
 
 #write python dict to a file
 mydict={'a':1,'b':2,'c':3}
+#'wb' stands for 'write and binary'. Pickle file is a binary file, thus, when you use write arguement, you need to use 'wb' instead of 'w'
 output= open('myfile.pkl','wb')
 pickle.dump(mydict,output)
 output.close()
@@ -11,7 +12,6 @@ output.close()
 file= open('myfile.pkl','rb')
 d = pickle.load(file)
 file.close()
-
 print(d)
 print(type(d))
 
