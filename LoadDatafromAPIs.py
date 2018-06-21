7d1511959c560f4bc69b284ea1315f8ddb54ecd9
celinexuzhang <celinezhang1991@outlook.com>
2018-06-20 22:56:08 -0400

    LoadDatafromAPI

--- LoadDatafromAPIs.py
+++ LoadDatafromAPIs.py
@@ -17,3 +17,32 @@ r=requests.get(url)
 json_data=r.json()
 for key in json_data.keys():
     print(key + ':', json_data[key])
+
+# Assign URL to variable: url
+url='http://www.omdbapi.com/?t=social+network&apikey=ff21610b'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+text=r.json()
+# Print the text of the response
+for key in text.keys():
+    print(key + ':',text[key])
+
+#Wikipedia API
+#Assign the relevant URL to the variable url.
+#Apply the json() method to the response object r and store the resulting dictionary in the variable json_data.
+#The variable pizza_extract holds the HTML of an extract from Wikipedia's Pizza page as a string; use the function print() to print this string to the shel
+# Assign URL to variable: url
+url='https://en.wikipedia.org/w/api.php?action=query&prop=extracts&format=json&exintro=&titles=pizza'
+
+# Package the request, send the request and catch the response: r
+r = requests.get(url)
+
+# Decode the JSON data into a dictionary: json_data
+json_data=r.json()
+#uery will return nested JSONs, that is, JSONs with JSONs
+for key in json_data.keys():
+    print(key + ':', json_data[key])
+# Print the Wikipedia page extract
+pizza_extract = json_data['query']['pages']['24768']['extract']
+print(pizza_extract)
